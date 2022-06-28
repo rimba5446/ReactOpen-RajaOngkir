d71bd981ce34dbb91256fe9310b139cc2ceaa5b6
Rimba5446 <lancarjaya1928@gmail.com>
2022-06-29 00:53:29 +0700

    React
    ReactOpen

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Check from "./components/Check";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Check />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api/api.tsx
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const API = axios.create({
+  baseURL: "https://enchanting-modern-glass.glitch.me/api",
+});
